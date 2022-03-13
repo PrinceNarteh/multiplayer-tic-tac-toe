f19db3216e90c4d4fe54a323bf5bf9a897b3c71e
Prince Narteh <prinarteh@gmail.com>
2022-03-13 21:06:07 +0000

    add globalStyle component

---
+++ client/src/styles/globalStyle.ts
@@ -0,0 +1,9 @@
+import {globalStyle} from 'styled-components'
+
+export const GlobalStyle = globalStyle`
+    *, *::before, *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`
