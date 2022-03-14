c24b6eea74bfda7c063009b9bb0ff2fb82df326b
Prince Narteh <prinarteh@gmail.com>
2022-03-14 02:37:53 +0000

    adds new client to fix package.json not present

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
-import { useEffect } from "react";
-import { io } from "socket.io-client";
-import { AppContainer, WelcomeText } from "./styles";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const connect = () => {
-    const socket = io('http://localhost:4000');
-
-    socket.on("connect", () => {
-      socket.emit('custom event', {name: "Prince", age: 20 })
-    })
-  }
-
-  useEffect(() => {
-    connect()
-  }, [])
-
   return (
-    <AppContainer>
-      <WelcomeText>Let's Play Tic Tac Toe</WelcomeText>
-    </AppContainer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- client/src/styles/globalStyle.ts
+++
@@ -1,9 +0,0 @@
-import {globalStyle} from 'styled-components'
-
-export const GlobalStyle = globalStyle`
-    *, *::before, *::after {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-`
