85f99e7aa3307b82e85603a699abea1f72cac1a9
Prince Narteh <prinarteh@gmail.com>
2022-03-17 06:48:25 +0000

    adds logic to check the state of the game

--- client/src/services/gameService.ts
+++ client/src/services/gameService.ts
@@ -1,4 +1,5 @@
 import { Socket } from "socket.io-client";
+import { IPlayMatrix, IStartGame } from "../components/Game";
 
 class GameService {
   async joinGameRoom(socket: Socket, roomId: string) {
@@ -7,6 +8,26 @@ class GameService {
       socket.on("room_join_success", () => resolve(true));
       socket.on("room_join_error", ({ error }) => reject(error));
     });
+  }
+
+  async updateGame(socket: Socket, gameMatrix: IPlayMatrix) {
+    socket.emit("update_game", { matrix: gameMatrix });
+  }
+
+  async onGameUpdate(socket: Socket, listener: (matrix: IPlayMatrix) => void) {
+    socket.on("on_game_update", ({ matrix }) => listener(matrix));
+  }
+
+  async onStartGame(socket: Socket, listener: (options: IStartGame) => void) {
+    socket.on("start_game", listener);
+  }
+
+  async gameWin(socket: Socket, message: string) {
+    socket.emit("game_win", { message });
+  }
+
+  async onGameWin(socket: Socket, listener: (message: string) => void) {
+    socket.on("on_game_win", ({ message }) => listener(message));
   }
 }
 
